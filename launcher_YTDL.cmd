caa8563d1113935e1862acc29496d08ec05506a3
Vintets <vintets@mail.ru>
2025-06-03 13:16:42 +0300

    Fix activate venv in bat

--- launcher_YTDL.cmd
+++ launcher_YTDL.cmd
@@ -1,7 +1,9 @@
 @echo off
 @color 71
 
+
 REM cd %~dp0
+CALL "%~dp0venv\Scripts\activate.bat"
 start /B /D "%~dp0venv\Scripts" "python.exe" "%~dp0YouTubeDl_GUI.py"
 REM start /B "" "%~dp0venv\Scripts\python.exe" "%~dp0YouTubeDl_GUI.py"
 REM pause
