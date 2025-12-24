23b0f82debf7299b1e36ea7f281417dbee8c4563
Vintets <vintets@mail.ru>
2025-12-24 16:40:38 +0300

    New version colorprint module

--- accessory/colorprint.py
+++ accessory/colorprint.py
@@ -1,24 +1,80 @@
 #!/home/admin/venv_flask3/bin/python3
 # -*- coding: utf-8 -*-
 
-import os
 import re
 import sys
 PLATFORM = sys.platform
 if PLATFORM == 'win32':
-    from ctypes import windll
+    from ctypes import byref, POINTER, Structure, windll, wintypes
     _stdout_handle = windll.kernel32.GetStdHandle(-11)
     _SetConsoleTextAttribute = windll.kernel32.SetConsoleTextAttribute
+    COORD = wintypes._COORD
+
+
+class WinStyle(object):
+    NORMAL              = 0x00  # dim text, dim background  # noqa E221
+    BRIGHT              = 0x08  # bright text, dim background  # noqa E221
+    BRIGHT_BACKGROUND   = 0x80  # dim text, bright background  # noqa E221
+
+
+if PLATFORM == 'win32':
+    class ConsoleScreenBufferInfo(Structure):
+        """struct in wincon.h."""
+        _fields_ = [
+            ('dwSize', COORD),
+            ('dwCursorPosition', COORD),
+            ('wAttributes', wintypes.WORD),
+            ('srWindow', wintypes.SMALL_RECT),
+            ('dwMaximumWindowSize', COORD),
+        ]
+
+        def __str__(self) -> str:
+            return '(%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d)' % (
+                self.dwSize.Y, self.dwSize.X,
+                self.dwCursorPosition.Y, self.dwCursorPosition.X,
+                self.wAttributes,
+                self.srWindow.Top, self.srWindow.Left, self.srWindow.Bottom, self.srWindow.Right,
+                self.dwMaximumWindowSize.Y, self.dwMaximumWindowSize.X
+            )
+
+    def get_console_screen_buffer_info() -> ConsoleScreenBufferInfo:
+        _GetConsoleScreenBufferInfo = windll.kernel32.GetConsoleScreenBufferInfo  # noqa N806
+        _GetConsoleScreenBufferInfo.argtypes = [
+            wintypes.HANDLE,
+            POINTER(ConsoleScreenBufferInfo),
+        ]
+        _GetConsoleScreenBufferInfo.restype = wintypes.BOOL
+
+        csbi = ConsoleScreenBufferInfo()
+        _GetConsoleScreenBufferInfo(_stdout_handle, byref(csbi))
+        return csbi
+
+    def get_win_default_attributes() -> dict[str, int]:
+        _default = get_console_screen_buffer_info().wAttributes
+        # print(_default)
+        win_default_attributes = {
+                'attr': _default,
+                'fore': _default & 7,
+                'back': (_default >> 4) & 7,
+                'style': _default & (WinStyle.BRIGHT | WinStyle.BRIGHT_BACKGROUND)
+                }
+        return win_default_attributes
+
+    WIN_DEFAULT_ATTRIBUTES = get_win_default_attributes()
 
 
 def _pr(cstr: str, force_linux: bool = False) -> None:
     clst = cstr.split('^')
-    color = 0x0001
+    color = 20
     for cstr in clst:
-        dglen = re.search(r'\D', cstr).start()
-        if dglen:
-            color = int(cstr[:dglen])
-        text = cstr[dglen:]
+        match = re.search(r'\D', cstr)
+        if match is not None:
+            dglen = match.start()
+            if dglen:
+                color = int(cstr[:dglen])
+            text = cstr[dglen:]
+        else:
+            text = cstr
         text = text.replace('\u0456', 'i')
         if text[:1] == '_':
             text = text[1:]
@@ -61,7 +117,7 @@ def colors_win() -> dict[int, str]:
             }
 
 
-def colors_win2linux() -> dict[int, str]:
+def colors_win2linux() -> dict[int, int]:
     return {
             0 : 30,  # noqa: E203
             1 : 34,  # noqa: E203
@@ -87,14 +143,14 @@ def colors_win2linux() -> dict[int, str]:
 
 def _dafault_color(force_linux: bool) -> int:
     # цвет по умолчанию: 20
-    # для windows это 1, для linux - сброс
+    # для windows это 1?, для linux - сброс
     def_color = 1
     if 'linux' in PLATFORM:
         def_color = 0
     elif force_linux:
         def_color = 20
     elif PLATFORM == 'win32':
-        def_color = 1
+        def_color = WIN_DEFAULT_ATTRIBUTES['fore']
     return def_color
 
 
@@ -105,7 +161,7 @@ def _set_color(color: int, force_linux: bool = False) -> str:
     if ('linux' in PLATFORM) or force_linux:
         prefix_color = '\033[%(col_linux)sm' % {'col_linux': colors_win2linux()[color]}
     elif PLATFORM == 'win32':
-        _SetConsoleTextAttribute(_stdout_handle, color | 0x0070)  # 78
+        _SetConsoleTextAttribute(_stdout_handle, color | WIN_DEFAULT_ATTRIBUTES['back'] * 16)  # 0x0070
     return prefix_color
 
 
@@ -120,10 +176,11 @@ if __name__ == '__main__':
 
     __author__ = 'master by Vint'
     __title__ = '--- colorprint ---'
-    __version__ = '4.0.2'
+    __version__ = '4.1.2'
     __copyright__ = 'Copyright 2024 (c)  bitbucket.org/Vintets'
     authorship(__author__, __title__, __version__, __copyright__, width=_width)
 
+    print(WIN_DEFAULT_ATTRIBUTES)  # noqa: T201
     for col in range(16):
         color_win = {
                 'color': col,
